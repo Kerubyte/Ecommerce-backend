60b9a4ac775bebce6e3be6483824540abc00ef3c
Kerubyte <kerubyte@gmail.com>
2022-02-10 00:05:14 +0100

    Refactor RootResponse.kt

--- src/main/kotlin/com/kerubyte/common/util/RootResponse.kt
+++ src/main/kotlin/com/kerubyte/common/util/RootResponse.kt
@@ -2,16 +2,15 @@ package com.kerubyte.common.util
 
 import io.ktor.http.*
 
-interface RootResponse<T : Any> {
+interface RootResponse<T : Any>
 
-    data class SuccessResponse<T : Any>(
-        val statusCode: HttpStatusCode,
-        val data: T? = null,
-        val message: String? = null
-    ) : RootResponse<T>
+data class SuccessResponse<T : Any>(
+    val statusCode: HttpStatusCode,
+    val data: T? = null,
+    val message: String? = null
+) : RootResponse<T>
 
-    data class ErrorResponse<T : Any>(
-        val statusCode: HttpStatusCode,
-        val message: String? = null
-    )
-}
+data class ErrorResponse<T : Any>(
+    val statusCode: HttpStatusCode,
+    val message: String? = null
+) : RootResponse<T>
